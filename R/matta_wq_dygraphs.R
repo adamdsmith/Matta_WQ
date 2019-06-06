e6b138a2c5ecacdb90285b87e0adadd42f8de9d6
Adam Smith <avianmigration@gmail.com>
2019-06-06 16:21:38 -0400

    Change to wide format with replicates IDd

--- R/matta_wq_dygraphs.R
+++ R/matta_wq_dygraphs.R
@@ -1,70 +1,97 @@
-
 # Load required packages
 if (!("pacman" %in% installed.packages())) install.packages("pacman", quiet = TRUE)
 if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
 if (!requireNamespace("nrsmisc", quietly = TRUE)) devtools::install_github("adamdsmith/nrsmisc")
 pacman::p_load(ggplot2, viridis, readr, readxl, dplyr, tidyr, lubridate, timetk, xts, dygraphs,
                htmlwidgets, manipulateWidget)
-
+adj_lt <- function(x, na.rm = FALSE) {
+  if (is.numeric(x)) return(x)
+  has_lt <- grepl("<", x)
+  mod_x <- as.numeric(gsub("<", "", x))
+  mod_x <- mod_x * (1 - (0.5 * has_lt))
+  mod_x
+}
 # Water quality metrics from grab samples at USGS gages on Lake Mattamuskeet NWR.
 
 # Historic samples (1981 - 2017)
-dwr <-  readxl::read_xlsx("Data/DWR_1981-2017_Surface main and qaqc.xlsx", sheet = "DWRfinal") %>%
+old <-  readxl::read_xlsx("Data/DWR_1981-2017_Surface main and qaqc.xlsx", sheet = "DWRfinal") %>%
   # Put all samples at noon...
-  mutate(date = ymd_hm(paste(Date, "12:00"), tz = "Etc/GMT+5"),
+  mutate(date = ymd_hm(paste(Date, "12:00"), tz = "Etc/GMT-5"),
          TP = TP * 1000) %>% # convert to micrograms/L
-  select(date, basin = `Lake side`,
-         chla = starts_with("Chl"), # micrograms/L
-         TN, # mg/L
-         susp_res = `Suspended Residue`, #mg/L
-         TP, # mg/L
-         turbidity = Turbidity) %>% # NTUs
-  gather("variable", "value", -date, -basin)
+  select(date, basin = `Lake side`, chla = starts_with("Chl"),
+         TKN, NH3, NOx, TN, res_susp_total = `Suspended Residue`,
+         res_susp_vol = `Suspended Residue_Volatile`,
+         res_susp_fixed = `Suspended Residue_Fixed`,
+         res_total = `Total Residue`,  res_total_vol = `Total Residue_Volatile`,
+         res_total_fixed = `Total Residue_Fixed`, TP, turbidity = Turbidity) %>%
+  # Fix "less than, <" values
+  mutate_at(vars(chla:turbidity), adj_lt) %>%
+  # Denote replicate measurements in a basin on a given date
+  group_by(date, basin) %>% mutate(rep = 1:n())
 
 # New ~ annual output format from ISB lab
-lab <- readxl::read_xlsx("Data/2018 Lake Mattamuskeet Data from ISB Lab.xlsx") %>%
+# Will need to generalize this when multiple spreadsheets become available...
+new_lab <- readxl::read_xlsx("Data/2018 Lake Mattamuskeet Data from ISB Lab.xlsx") %>%
   select(date = CDAT, basin = LCOD, variable = ACOD, value = ACOM) %>%
   mutate(basin = ifelse(basin == "0208458892", "W", "E"),
-         date = ymd_hm(paste(date, "12:00"), tz = "Etc/GMT+5"),
-         variable = case_when(
-           variable == "TURBIDITY"        ~ "turbidity",
-           variable == "PHOSTOTP_LIQ"     ~ "TP",
-           variable == "TKNN_LIQ"         ~ "TN",
-           variable == "NO2&NO3_LIQ"      ~ "TN",
-           variable == "RESSUS_WET"       ~ "susp_res",
-           variable == "CHLOROPHYLLA_LIQ" ~ "chla",
-           TRUE                           ~ NA_character_)) %>%
-  filter(!is.na(variable)) %>%
-  # Coerce value column to numeric (some records had values like 'X3', etc.)
-  mutate(value = as.numeric(value),
+         date = ymd_hm(paste(date, "12:00"), tz = "Etc/GMT-5"),
+         variable = ifelse(grepl("CHLORO", variable), "chla", variable),
+         # Remove "less than signs" (return half of value) and coerce to numeric
+         # Some records had values like 'X3', etc.
+         value = adj_lt(value),
          # Convert TP to micrograms/L
-         value = ifelse(variable == "TP", value * 1000, value)) %>%
-  # Add multiple variables to get total N
+         value = ifelse(variable == "PHOSTOTP_LIQ", value * 1000, value)) %>%
   group_by(date, basin, variable) %>%
-  summarise(value = sum(value, na.rm = TRUE))
+  mutate(rep = 1:n()) %>%
+  spread(variable, value) %>%
+  mutate(TN = TKNN_LIQ + `NO2&NO3_LIQ`) %>%
+  select(date, basin, chla, TKN = TKNN_LIQ, NH3 = NH3N_LIQ, NOx = `NO2&NO3_LIQ`,
+         TN, res_susp_total = RESSUS_WET, res_susp_vol = RESSUSVOL_WET,
+         res_susp_fixed = RESSUSFIX_WET, res_total = RESTOT_WET,
+         res_total_vol = RESTOTVOL_WET, res_total_fixed = RESTOTFIX_WET,
+         TP = PHOSTOTP_LIQ, turbidity = TURBIDITY, rep)
 
 # Attempt to flag potentially problematic dates
 source("R/flag_observations.R")
 check_labs(new_lab)
 
-standards <- data.frame(variable = c("chla", "TN", "TP", "susp_res", "turbidity"),
-                        axis = c("Chlorophyll<i><sub>a</sub></i><br>(&mu;g/L)",
-                                  "Total nitrogen<br> (mg/L)",
-                                  "Total phosphorus<br>(&mu;g/L)",
-                                  "Total susp. solids<br>(mg/L)",
-                                  "Turbidity<br>(NTUs)"),
-                        min = c(0, 0.32, 8, 0, 0),
-                        max = c(40, 0.41, 20, 15, 25),
-                        stringsAsFactors = FALSE)
+wq <- bind_rows(old, new_lab) %>%
+  arrange(date, basin)
 
-vars <- unique(wq$variable)
-wq_ts <- lapply(vars, function(v) {
-  message(v)
-  std <- filter(standards, variable == v)
-  tmp <- filter(wq, variable == v) %>% tibble::rowid_to_column() %>%
-    spread(basin, value) %>% select(date, E:W) %>%
-    arrange(date, W, E) %>%  tk_xts(tzone = "Etc/GMT+5") %>%
-    xts::make.time.unique(eps = 1800)
+# Create standards and/or label data.frame
+poi_standards <- data.frame(
+  variable = c("chla", "TN", "TP", "res_susp_total", "turbidity",
+               "TKN", "NH3", "NOx", "res_susp_vol", "res_susp_fixed",
+               "res_total", "res_total_vol", "res_total_fixed"),
+  axis = c("Chlorophyll<i><sub>a</sub></i><br>(&mu;g/L)",
+           "Total nitrogen<br>(mg/L)",
+           "Total phosphorus<br>(&mu;g/L)",
+           "Susp. solids - total<br>(mg/L)",
+           "Turbidity<br>(NTUs)",
+           "Total Kjeldahl nitrogen<br>(mg/L)",
+           "Ammonia<br>(mg/L)",
+           "Nitrites/Nitrates<br>(mg/L)",
+           "Susp. solids - volatile<br>(mg/L)",
+           "Susp. solids - fixed<br>(mg/L)",
+           "Solids - total<br>(mg/L)",
+           "Solids - total volatile<br>(mg/L)",
+           "Solids - total fixed<br>(mg/L)"),
+  min = c(0, 0.32, 8, 0, 0, rep(NA, 8)),
+  max = c(40, 0.41, 20, 15, 25, rep(NA, 8)),
+  stringsAsFactors = FALSE)
+
+###################################################
+## CORE WATER QUALITY PARAMETERS OF INTEREST (POI)
+###################################################
+
+core_poi <- c("turbidity", "TP", "TN", "res_susp_total", "chla")
+
+wq_core_dy <- lapply(core_poi, function(v) {
+  std <- filter(poi_standards, variable == v)
+  tmp <- select(wq, date, basin, rep, !!v) %>% ungroup() %>%
+    spread(basin, !!v) %>% select(date, E:W) %>%
+    tk_xts(tzone = "Etc/GMT-5", select = c(W, E), date_var = date) %>%
+    xts::make.time.unique(eps = 1800) # Arbitrarily space replicates 30 mins apart to visualize
   init_window <- as.character(c(Sys.time() - years(5), Sys.time()))
   tmp_dy <- dygraph(tmp, group = "water_quality") %>%
     dyOptions(colors = c("#b2182b", "#2166ac"), axisLineWidth = 2, connectSeparatedPoints = FALSE,
@@ -78,7 +105,8 @@ wq_ts <- lapply(vars, function(v) {
     dyCSS("css/matta_wq.css")
 })
 
-all <- manipulateWidget::combineWidgets(list = wq_ts, ncol = 1)
-saveWidget(all, "Mattamuskeet_water_quality_dygraph.html", title = "Mattamuskeet Water Quality")
+core_poi_dy <- manipulateWidget::combineWidgets(list = wq_core_dy, ncol = 1)
+saveWidget(core_poi_dy, "Mattamuskeet_water_quality_dygraph.html", title = "Mattamuskeet Water Quality")
 file.rename("Mattamuskeet_water_quality_dygraph.html", "./docs/Mattamuskeet_water_quality_dygraph.html")
+file.remove("./docs/index.html")
 file.copy("./docs/Mattamuskeet_water_quality_dygraph.html", "./docs/index.html")
