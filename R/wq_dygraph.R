5631e7a1579982be6ea331741fb202e498196fd9
Adam Smith <adam_d_smith@fws.gov>
2019-11-18 15:05:18 -0500

    generalize dygraph plotting into fxn

---
+++ R/wq_dygraph.R
@@ -0,0 +1,18 @@
+wq_dygraph <- function(data, std) {
+  dy <- dygraph(data, group = "water_quality") %>%
+    dyOptions(colors = c("#b2182b", "#b2182b", "#2166ac", "#2166ac"),
+              axisLineWidth = 2, connectSeparatedPoints = TRUE) %>%
+    dySeries("W", label = "West", drawPoints = TRUE, pointSize = 3, pointShape = "square",
+             strokeWidth = 0) %>%
+    dySeries("W_loess", drawPoints = FALSE) %>%
+    dySeries("E", label = "East", drawPoints = TRUE, pointSize = 3, pointShape = "square",
+             strokeWidth = 0) %>%
+    dySeries("E_loess", drawPoints = FALSE) %>%
+    dyAxis("y", label = std$axis) %>%
+    dyShading(from = std$min, to = std$max, axis = "y", color = "#addd8e") %>%
+    dyLegend(show = "follow", width = 200) %>%
+    dyRangeSelector(height = 20, strokeColor = "", dateWindow = init_window) %>%
+    dyCSS("css/matta_wq.css")
+  dy
+}
+
