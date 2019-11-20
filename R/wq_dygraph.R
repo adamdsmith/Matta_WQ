26e3a3926a321d56fa6afdc3773652a029c7d7a2
Adam Smith <adam_d_smith@fws.gov>
2019-11-20 16:31:03 -0500

    Allow fixed y-axis scale

--- R/wq_dygraph.R
+++ R/wq_dygraph.R
@@ -1,4 +1,4 @@
-wq_dygraph <- function(data, std) {
+wq_dygraph <- function(data, std, fix_y_range = NULL) {
   dy <- dygraph(data, group = "water_quality") %>%
     dyOptions(colors = c("#b2182b", "#b2182b", "#2166ac", "#2166ac"),
               axisLineWidth = 2, connectSeparatedPoints = TRUE) %>%
@@ -8,7 +8,7 @@ wq_dygraph <- function(data, std) {
     dySeries("E", label = "East", drawPoints = TRUE, pointSize = 3, pointShape = "square",
              strokeWidth = 0) %>%
     dySeries("E_loess", drawPoints = FALSE) %>%
-    dyAxis("y", label = std$dy_axis) %>%
+    dyAxis("y", label = std$dy_axis, valueRange = fix_y_range) %>%
     dyShading(from = std$min, to = std$max, axis = "y", color = "#addd8e") %>%
     dyLegend(show = "follow", width = 200) %>%
     dyRangeSelector(height = 20, strokeColor = "", dateWindow = init_window) %>%
